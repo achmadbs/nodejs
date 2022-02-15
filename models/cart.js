e869d79cdd81137658d2150ebc5cda693d802205
achmad <achmadbdrdn@gmail.com>
2022-02-15 20:36:44 +0700

    fix/delete-product

--- models/cart.js
+++ models/cart.js
@@ -40,6 +40,7 @@ module.exports = class Cart {
       if (err) return;
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
+      if (!product) return null;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
       );
