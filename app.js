9a1288bdafdd4940aece91a95cc2a864406e1087
achmad <achmadbdrdn@gmail.com>
2022-02-22 10:50:36 +0700

    feat/cart relation and cartItem

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.set('views', 'views');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cartItem');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -39,6 +41,10 @@ Product.belongsTo(User, {
   onDelete: 'CASCADE',
 });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 // use
 // authenticate instead
